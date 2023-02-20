196cfde600ba6f0825557a702938e5f4ff8e0bb9
SHIVAM SINGH <91870115+shivam8989@users.noreply.github.com>
2023-02-20 11:10:14 +0530

    Add files via upload
    Program to find a level of a binary tree

---
+++ Kth_Level_Of_Tree.java
@@ -0,0 +1,36 @@
+package Binary_Tree;
+
+public class Kth_Level_Of_Tree {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+    public static void KthLevel(Node root, int level , int k){
+        if(root == null){
+            return ;
+        }
+        if(level == k){
+            System.out.print(root.data+" ");
+            return ;
+        }
+        KthLevel(root.left,level+1,k);
+        KthLevel(root.right, level+1, k);
+    }
+    public static void main(String args[]){
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+        int k = 2;
+        KthLevel(root,1,2);
+    }
+}
